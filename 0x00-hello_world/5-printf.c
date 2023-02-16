4ae0c043a844ec965f7fb00139e9ed54951127e5
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-02-16 22:07:10 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,9 @@
-echo #!/bin/bash
+#include <stdio.h>
+/** 
+ * main - Entry
+ * Return: Always 0
+ * /
+int main(void){
+    printf("with proper grammar, bit the outcome is a piece of art,\n");
+    return (0);
+}
