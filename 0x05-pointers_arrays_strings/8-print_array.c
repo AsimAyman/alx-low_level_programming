a81aa7feaa1f26c19375c7d172438d20ce20bb5f
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-02-28 21:37:17 +0200

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,32 @@
 #include "main.h"
 
 /**
- * print_array - a function that prints n elements of an array
- * @a: array name
- * @n: is the number of elements OF the array to be printed
- * Return: a and n inputs
+ * rot13 - Entry point
+ * ONE if, TWO loops only...
+ * @n: input
+ * Return: decrypted string
  */
-void print_array(int *a, int n)
+char *rot13(char *n)
 {
-	int i;
+	int x, rot_c = 13, i = 0;
+	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
+		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
+		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
+		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
+		'Z', 'm', 'z'};
 
-	for (i = 0; i < (n - 1); i++)
+	while (n[i] != '\0')
 	{
-		printf("%d, ", a[i]);
-	}
-		if (i == (n - 1))
+		for (x = 0; x <= 51; x++)
 		{
-			printf("%d", a[n - 1]);
+			if (n[i] == toswap[x])
+			{
+				n[i] = n[i] + rot_c;
+				x = 51;
+			}
+			rot_c = rot_c * -1;
 		}
-			printf("\n");
+		i++;
+	}
+	return (n);
 }
