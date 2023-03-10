fe1099dc226db8825be7d7b9a85d3b694cda5ed8
assem <Assemayman136@gmail.com>
2023-03-10 08:41:23 -0800

    print the number of arrg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ * @argc: Count of the arg that function need
+ * @argv: Pointer of array of pointers containing strings enteringmain
+ * Return: Always 0(Success)
+ */
+int mian(int argc, char* argv[])
+{
+	printf("%d\n",argc);
+	return (0);
+}
