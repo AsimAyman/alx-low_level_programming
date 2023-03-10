02befbcdca94b207003342e932f0012e3014d106
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-03-10 19:51:39 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 (Success)
  */
-int main(int argc)		
+int main(int argc,char *argv[])		
 {		
-        //(void) argv; /*Ignore argv*/		
+(void) argv; /*Ignore argv*/		
 printf("%d\n",argc - 1);	
 return (0);		
 }
