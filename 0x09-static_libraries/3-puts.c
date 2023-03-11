f9bcde7103c0b8a7bda588ac86c0f12e5b77172b
assem <Assemayman136@gmail.com>
2023-03-11 08:11:22 -0800

    intial

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,6 @@
-include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
+#include "main.h"
+#include <stdio.h>
+void _puts(char *str){
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        
 }
-
