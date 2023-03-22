41bc1876051b254fd41d207adcfab67e639602bd
assem <Assemayman136@gmail.com>
2023-03-22 12:03:29 -0700

    ad

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,49 +1,33 @@
 #include <stdio.h>
-	#include <stdlib.h>
+#include <stdlib.h>
 
+/**
+ * main - program that prints the opcodes of its own main function
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ *
+ * Return: 0 on succes, 1 on argv != 2, 2 on negative bytes in argv
+ */
+int main(int argc, char **argv)
+{
+	int i, bytes;
+	char *mainaddr;
 
-	/**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int main(int argc, char *argv[])
-	{
-		int bytes, i;
-		char *arr;
+	if (argc != 2)
+		printf("Error\n"), exit(1);
 
+	bytes = atoi(argv[1]);
 
-		if (argc != 2)
-		{
-			printf("Error\n");
-			exit(1);
-		}
+	if (bytes < 0)
+		printf("Error\n"), exit(2);
 
+	mainaddr = (char *)main;
 
-		bytes = atoi(argv[1]);
+	for (i = 0; i < bytes - 1; i++)
+		printf("%02hhx ", mainaddr[i]);
 
+	printf("%02hhx\n", mainaddr[i]);
 
-		if (bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-
-		arr = (char *)main;
-
-
-		for (i = 0; i < bytes; i++)
-		{
-			if (i == bytes - 1)
-			{
-				printf("%02hhx\n", arr[i]);
-				break;
-			}
-			printf("%02hhx ", arr[i]);
-		}
-		return (0);
-	}
-
+	return (0);
+}
