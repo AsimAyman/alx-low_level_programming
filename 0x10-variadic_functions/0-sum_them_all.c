a1148c8777f29f2359476229dff7a762445becf2
assem <Assemayman136@gmail.com>
2023-03-23 12:32:11 -0700

    sum hot fic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,7 +7,7 @@ int sum_them_all(const unsigned int n, ...)
    if(n ==0)return 0;
    va_list ptr;
    va_start(ptr,n);
-   for(int i=0:i<n;i++)
+   for(int i=0;i<n;i++)
 	   sum+=va_arg(ptr,int);
    va_end(ptr);
    return sum;
