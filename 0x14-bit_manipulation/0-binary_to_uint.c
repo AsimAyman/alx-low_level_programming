ab412542bbf34fa05a1b8d112b3ed823361d2ce3
assem <Assemayman136@gmail.com>
2023-04-07 05:16:14 -0700

    frist fun

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+unsigned int binary_to_uint(const char *b){
+    int value =0,i =0,j =strlen(b)-1;
+    for (i;i<strlen(b);i++){
+        int currentDigit =0;
+        if(b[i]=='1') currentDigit =1;
+        else currentDigit =0;
+        printf("%d * %d \n",currentDigit,j);
+         value += currentDigit * (int)pow(2,j--);
+    }
+     
+        return value;
+}
