05df26957529ec883a34e604c5812a2464bfc19b
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-04-08 20:27:10 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,39 @@
 #include "main.h"
-/**
- * bianry_to_uint: converts a binary number to an unsigned int.
- * @b: is a pointer to an array of 1 and 0.
- * Return: equivlent  integer value or 0 in the case of error
- */
-unsigned int binary_to_uint(char *b){
-         unsigned int res = 0,one ,i;
-	 int l=0;
-	 if(b==0)return 0;
-	 while (b[i]!=0){
-	   if(b[l]!='1'&&b[l]!='0')reutrn 0;
-	   l++;
-	 }
-	 l--;
-	 for(i =0;l>=0;i++,l--){
-		 if(b[l]=='1')one=1;
-		 else one=0;
-		 one=one<<i;
-		 res=res |one;
-	 }
-	 return res;
 
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: b is pointing to a string of 0 and 1 chars
+ *
+ * Return: Converted number or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * or if b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0, one, i;
+	int l = 0;
+
+	if (b == 0)
+		return (0);
+
+
+	while (b[l] != 0)
+	{
+		if (b[l] != '0' && b[l] != '1')
+			return (0);
+		l++;
+	}
+
+	l = l - 1;
+
+	for (i = 0; l >= 0; i++, l--)
+	{
+		if (b[l] == '0')
+			one = 0;
+		else if (b[l] == '1')
+			one = 1;
+		one = one << i;
+		res = res | one;
+	}
+	return (res);
 }
