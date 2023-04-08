3661148ffd7ea0ff563e43a49bf957b00fac0f14
assem <Assemayman136@gmail.com>
2023-04-08 11:25:53 -0700

    convert to decem

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
 #include "main.h"
-unsigned int binary_to_uint(const char *b){
-    int value =0,i =0,j =strlen(b)-1;
-    for (i;i<strlen(b);i++){
-        int currentDigit =0;
-        if(b[i]=='1') currentDigit =1;
-        else currentDigit =0;
-        printf("%d * %d \n",currentDigit,j);
-         value += currentDigit * (int)pow(2,j--);
-    }
-     
-        return value;
+/**
+ * bianry_to_uint: converts a binary number to an unsigned int.
+ * @b: is a pointer to an array of 1 and 0.
+ * Return: equivlent  integer value or 0 in the case of error
+ */
+unsigned int binary_to_uint(char *b){
+         unsigned int res = 0,one ,i;
+	 int l=0;
+	 if(b==0)return 0;
+	 while (b[i]!=0){
+	   if(b[l]!='1'&&b[l]!='0')reutrn 0;
+	   l++;
+	 }
+	 l--;
+	 for(i =0;l>=0;i++,l--){
+		 if(b[l]=='1')one=1;
+		 else one=0;
+		 one=one<<i;
+		 res=res |one;
+	 }
+	 return res;
+
 }
