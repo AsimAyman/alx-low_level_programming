c4b2acf788fdb0c873491534e67b7984103d85a1
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-04-08 21:15:06 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,43 @@
 #include "main.h"
-#include <stdlib.h>
 /**
- * print_binary: convertes binary to dicimal to binary.
- * @n: binary to be converted,
- * Retrun: non just print the equivalent binary 
+ * rev_bin - prints binary representation of number with & and Shift Right
+ * operators, but uses recursion to reverse the result
+ *
+ * @n: decimal number
+ * @check: checks for special case number 0
+ *
+ * Return: No return
  */
-void print_binary(unsigned long int n){
-    int reminder=0,divider=n,l=0;
-    int *num =malloc(l*4);
-    while(1){
-        reminder = divider%2;
-        if(reminder ==0)num[l]=0;
-        else num[l]=1;
-        divider /=2;
-        if(!divider)break;
-        l++;
-    } 
-    for(int i =l;i>=0;i--){
-       _putchar(num[i]);
-    }
-    free(num);
+void rev_bin(unsigned long int n, int check)
+{
+	if (check == 1)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n == 0)
+		return;
+
+	rev_bin(n >> 1, check);
+
+	if ((n & 1) == 0)
+		_putchar('0');
+
+	if ((n & 1) == 1)
+		_putchar('1');
+}
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: base 10 number
+ *
+ * Return: No return
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		rev_bin(n, 1);
+	else
+		rev_bin(n, 0);
 }
