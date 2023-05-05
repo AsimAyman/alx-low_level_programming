894f94db04562c5bd90da73d6860c79a61516068
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-05-05 22:27:38 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * rev_bin - prints binary representation of number with & and Shift Right
+ * operators, but uses recursion to reverse the result
+ *
+ * @n: decimal number
+ * @check: checks for special case number 0
+ *
+ * Return: No return
+ */
 void rev_bin(unsigned long int n, int check)
 {
 	if (check == 1)
@@ -7,7 +16,7 @@ void rev_bin(unsigned long int n, int check)
 		return;
 	}
 
-        if (n == 0)
+	if (n == 0)
 		return;
 
 	rev_bin(n >> 1, check);
@@ -18,6 +27,13 @@ void rev_bin(unsigned long int n, int check)
 	if ((n & 1) == 1)
 		_putchar('1');
 }
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: base 10 number
+ *
+ * Return: No return
+ *
+ */
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
