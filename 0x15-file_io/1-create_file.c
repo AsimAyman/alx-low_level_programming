301320f4cc6e9a8351f5028751b80789112a49f5
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-05-08 22:08:45 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,16 +1,5 @@
 #include "main.h"
 
-/**
- * create_file - creates a file
- *
- * @filename: name of the file to be created
- * @text_content: NULL terminated string to write to the file
- *
- * Return: 1 on success, -1 on failure (file cannot be created, written,
- * etc.). The created file must have permissions rw-------.
- * If the file already exists, do not change its permissions.
- * If filename is NULL, return -1. If text_content is NULL, create an empty file.
- */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, check_write, length = 0;
