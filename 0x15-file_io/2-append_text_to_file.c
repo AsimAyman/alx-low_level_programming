8a499c796e524d84868cec86be116f92eee657bf
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-05-08 22:22:36 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
     if (filename == NULL)
         return (-1);
     f = open(filename, O_APPEND | O_RDWR, 0664);
-    if (file == -1)
+    if (f == -1)
         return (-1);
 
     if (text_content != NULL)
