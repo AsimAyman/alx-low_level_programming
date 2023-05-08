46967b7a7b6173755db90ae5f5c57287c92cc55d
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-05-08 22:14:18 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,27 +9,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int fd, check_write, length = 0;
+	int fd, checkw, l = 0;
 
-if (filename == NULL)
-	return (-1);
+	if (filename == 0)
+		return (-1);
 
-fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-if (fd == -1)
-return (-1);
+	if (fd == -1)
+		return (-1);
 
-if (text_content)
-{
-while (text_content[length] != '\0')
-			length++;
-check_write = write(fd, text_content, length);
+	if (text_content)
+	{
+		while (text_content[l] != 0)
+			l++;
+		checkw = write(fd, text_content, l);
 
-if (check_write == -1)
-return (-1);
-}
+		if (checkw == -1)
+			return (-1);
+	}
 
-close(fd);
-return (1);
-	
+	close(fd);
+	return (1);
 }
