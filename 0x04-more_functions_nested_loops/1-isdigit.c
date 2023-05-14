76645fe6c3e227b09f5b46c8c626019032d0a347
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-05-14 15:02:11 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isdigit - checks if the c it digit or not
- *@c:is a value to be checked 
+ *@c: is a value to be checked 
  *Return: 1 if it is  digit and 0 if it is not.
  */
 int _isdigit(int c)
