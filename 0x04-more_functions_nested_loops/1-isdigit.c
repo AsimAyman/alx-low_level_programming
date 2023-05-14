a89e25d6e0c7f5e89ee1e3235941544dc5baf220
Asim Ayman <assemayman136@gmail.com>
2023-05-14 04:52:49 -0700

    hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,6 +3,7 @@
  *@c:is a value to be checked 
  *Returns: 1 if it is  digit and 0 if it is not.
  */
-int _isdigit(int c){
-    return (if(c <= 9 && c >= 0));
+int _isdigit(int c)
+{
+    return (if(c <= '9' && c >= '0'));
 }			   
