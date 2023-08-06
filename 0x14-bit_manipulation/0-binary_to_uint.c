67a376b8b6d069f9e832251ebef611b48c45e0c2
AsimAyman <Assemayman136@gmail.com>
2023-08-06 02:13:21 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,17 +1,19 @@
 unsigned int binary_to_uint(const char *b){
-   unsigned int reds=0,currentBit;
+   unsigned int res=0,currentBit;
    int l=0;
    if(b==0)return 0;
    while(b[l]!=0){
 
-           if(b[l]!="1"||b[l]!="0")return 0;
+           if(b[l]!='1'&&b[l]!='0')return 0;
            l++;
    }
    l--;
-   for(int i=0;l>0;i++,l--){
-     if[b[i]=="1")currentBit=1;
+   for(int i=0;l>=0;i++,l--){
+     if(b[i]=='1')currentBit=1;
      else currentBit=0;
-     currentBit <=l;
+     currentBit = currentBit << l;
+     //printf("%d\n",currentBit);
      res +=currentBit;
    }
    return res;
+}
