53d25a753004e9fef47a7a711e5958489660c9aa
AsimAyman <Assemayman136@gmail.com>
2023-08-06 01:55:34 -0700

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+unsigned int binary_to_uint(const char *b){
+   unsigned int reds=0,currentBit;
+   int l=0;
+   if(b==0)return 0;
+   while(b[l]!=0){
+
+           if(b[l]!="1"||b[l]!="0")return 0;
+           l++;
+   }
+   l--;
+   for(int i=0;l>0;i++,l--){
+     if[b[i]=="1")currentBit=1;
+     else currentBit=0;
+     currentBit <=l;
+     res +=currentBit;
+   }
+   return res;
