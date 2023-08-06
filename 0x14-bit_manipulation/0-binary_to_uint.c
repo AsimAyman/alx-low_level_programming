9ad5034bce9f781f589d937349a920c4ef15cfb8
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-08-06 12:57:59 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,21 +3,25 @@ unsigned int binary_to_uint(const char *b)
 unsigned int res = 0, currentBit;
 int l = 0;
 
-if (b == 0) 
+if (b == 0){
    return (0);
+}
 while (b[l] != 0)
 {
-if (b[l] != '1' && b[l] != '0')
+if (b[l] != '1' && b[l] != '0'){
    return 0;
+}   
 l++;
 }
 l--;
 for (int i = 0; l >= 0; i++, l--)
 {
-if (b[i] == '1')
+if (b[i] == '1'){
    currentBit = 1;
-else
+}   
+else{
    currentBit = 0;
+}
 currentBit = currentBit << l;
 res += currentBit;
 }
