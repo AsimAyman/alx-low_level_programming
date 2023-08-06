212a82773e45b0483a144a2751cffc6553bc9e71
AsimAyman <Assemayman136@gmail.com>
2023-08-06 05:10:26 -0700

    1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
 int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
-#endif
-=======
 unsigned int binary_to_uint(const char *b);
 #endif
-
->>>>>>> e46521c1b10ae515454d55d347a6574e18e00e57
